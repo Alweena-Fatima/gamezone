5daf56d8f6e6ddf15a5d216b10eb612df985da7e
Alweena-Fatima <alweenacse@gmail.com>
2025-06-11 17:27:18 +0530

    added box shadow when the side bar opton is active not ony on hover

--- src/components/Navoption.jsx
+++ src/components/Navoption.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './SideBar.css'
-function Navoption({item}) {
+function Navoption({item,navOnClick}) {
     return (
         <li>
-            <a href='#'> <i class={item.icon}></i> <span className='nameName'>{item.name}</span></a>
+            <a href='#' className={`${item.active? 'active': undefined}`}onClick={()=>navOnClick(item._id)}> <i class={item.icon}></i> <span className='nameName'>{item.name}</span></a>
         </li>
     )
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,9 +2,20 @@ import React, { useState } from 'react'
 import './SideBar.css'
 import NavoptionList from '../data/NavoptionList';
 import Navoption from './Navoption';
-function SideBar({ DarkMode, hide}) {
+function SideBar({ DarkMode, hide }) {
     const [navData, setNavData] = useState(NavoptionList); // keep this
+    const hancleNavOnclick = (id) => {
+        console.log(id)
+        const newNavData = navData.map(nav => {
+            nav.active = false;
+            if (nav._id == id) {//if nav id matched to the id of what we have clicked make it active
+                nav.active = true;
+            }
+            return nav
+        })
+        setNavData(newNavData);
 
+    }
     return (
         <>
             <div className={`sidebar ${DarkMode ? 'dark' : 'light'} ${hide ? 'collapsed' : ''}`}>
@@ -16,14 +27,14 @@ function SideBar({ DarkMode, hide}) {
                 <ul className='option'>
                     {
                         navData.filter(item => item.name !== 'Contact')
-                            .map(item => <Navoption key={item._id} item={item} />)
+                            .map(item => <Navoption key={item._id} item={item} navOnClick={hancleNavOnclick}/>)
                     }
                 </ul>
 
-                <ul className='Contact'>
+                <ul className='Contact' >
                     {
                         navData.filter(item => item.name === 'Contact')
-                            .map(item => <Navoption key={item._id} item={item} />)
+                            .map(item => <Navoption key={item._id} item={item} navOnClick={hancleNavOnclick}/>)
                     }
                 </ul>
 
